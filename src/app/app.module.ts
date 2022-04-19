d9c2ef3553663d8d20589453d4d56313a34b9804
Kiko Sopra <francisco.guirado@soprasteria.com>
2022-04-19 09:08:27 +0200

    fixed import issues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponentComponent } from './components/home-component/home-component.component';
+import { GameComponentComponent } from './components/game-component/game-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponentComponent,
+    GameComponentComponent,
   ],
   imports: [
     BrowserModule,
