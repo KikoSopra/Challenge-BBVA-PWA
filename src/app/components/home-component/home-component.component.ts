58ce691c4fa98a30c2834de1a8018d4e1845e802
Kiko Sopra <francisco.guirado@soprasteria.com>
2022-04-19 13:31:38 +0200

    fixed localStorage

--- src/app/components/home-component/home-component.component.ts
+++ src/app/components/home-component/home-component.component.ts
@@ -1,23 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Player } from '../../models/player'
+import { Player } from '../../models/player';
+import { GameService } from '../../services/game.service';
 
 @Component({
   selector: 'app-home-component',
   templateUrl: './home-component.component.html',
-  styleUrls: ['./home-component.component.css']
+  styleUrls: ['./home-component.component.css'],
 })
 export class HomeComponentComponent implements OnInit {
+  constructor(public gameService: GameService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   player: Player = {
     name: '',
     score: 0,
-    maxScore: 0
-  }
+    maxScore: 0,
+  };
 
+  addPlayer(name: HTMLInputElement) {
+    this.gameService.addPlayer({
+      name: name.value,
+      score: 0,
+      maxScore: 0,
+    });
+  }
 }
--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { Player } from '../models/player';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GameService {
+  players: Player[];
 
-  constructor() { }
+  constructor() {
+    this.players = [];
+  }
+
+  addPlayer(player: Player) {
+    if (localStorage.getItem('players') === null) {
+      this.players.push(player);
+      localStorage.setItem('players', JSON.stringify(this.players));
+    } else {
+      let localStorageItem = JSON.parse(
+        localStorage.getItem('players') || '[]'
+      );
+      let item = localStorageItem.find(
+        (item: Player) => item.name === player.name
+      );
+      if (item) {
+        alert('Player already exists');
+      } else {
+        this.players.push(player);
+        localStorage.setItem('players', JSON.stringify(this.players));
+      }
+    }
+  }
 }
